95a044439e544ff4922594cdf0028cf82f5a1177
Sebastian <sebastian.g.diaz@accenture.com>
2022-10-11 17:06:30 -0300

    start mern code unic files

--- 02mern/back/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import postRoutes from './routes/postRutes.js'
-
-const app = express()
-
-app.use(postRoutes) 
-
-app.listen(3000)
-console.log('rodando oioio', 3000)
